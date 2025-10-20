cb82f3902037b70d6b92263de7947d03614df242
Danukeru <danukeru@retrocogni.com>
2025-10-20 16:55:54 +0000

    add build parameter to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+if [[ -z "${APP_ENV}" ]]; then APP_LOSP_ENV=UNSET; fi
 
 clang-20 -O1 -emit-llvm decodeStub.c -c -o decodeStub.bc && llvm-dis-20 decodeStub.bc
 
-/usr/bin/clang++-20 -shared -I /usr/include/llvm-20/ -I /usr/include/llvm-c-20/ -O0 -o ObfString.so StrObfuscate.cpp -fPIC
+/usr/bin/clang++-20 -shared -I /usr/include/llvm-20/ -I /usr/include/llvm-c-20/ -O0 -o ObfString.so StrObfuscate.cpp -fPIC -D $APP_LOSP_ENV
